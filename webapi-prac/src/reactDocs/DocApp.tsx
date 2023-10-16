f4aa49e62f50a678975b7d06f859d1371afb733f
Ashes814 <1534661514@qq.com>
2023-10-16 20:01:53 +0800

    update LSGI521 Lab1

--- webapi-prac/src/reactDocs/DocApp.tsx
+++ webapi-prac/src/reactDocs/DocApp.tsx
@@ -1,43 +1,84 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+// import AddTask from "./AddTask.js";
+// import TaskList from "./TaskList.js";
 
-function Panel({ title, children, isActive, onShow }) {
-  return (
-    <section className="panel">
-      <h3>{title}</h3>
-      {isActive ? <p>{children}</p> : <button onClick={onShow}>Show</button>}
-    </section>
-  );
-}
+export default function TaskApp() {
+  // const [tasks, setTasks] = useState(initialTasks);
 
-export default function Accordion() {
-  const [activeIndex, setIsActive] = useState(0);
+  // function handleAddTask(text) {
+  //   setTasks([
+  //     ...tasks,
+  //     {
+  //       id: nextId++,
+  //       text: text,
+  //       done: false,
+  //     },
+  //   ]);
+  // }
+  // function handleAddTask(text) {
+  //   dispatchEvent({
+  //     type: "added",
+  //     id: nextId++,
+  //     text: text,
+  //   });
+  // }
+
+  // function handleChangeTask(task) {
+  //   dispatch({
+  //     type: "changed",
+  //     task: task,
+  //   });
+  // }
+
+  // function handleDeleteTask(taskId) {
+  //   dispatch({
+  //     type: "deleted",
+  //     id: taskId,
+  //   });
+  // }
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
+  function handleStart() {
+    // Start counting.
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    intervalRef.current = setInterval(() => {
+      // Update the current time every 10ms.
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
 
   return (
     <>
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel
-        title="About"
-        isActive={activeIndex === 0}
-        onShow={() => {
-          setIsActive(0);
-        }}
-      >
-        With a population of about 2 million, Almaty is Kazakhstan's largest
-        city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel
-        title="Etymology"
-        isActive={activeIndex === 1}
-        onShow={() => {
-          setIsActive(1);
-        }}
-      >
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
-        "apple" and is often translated as "full of apples". In fact, the region
-        surrounding Almaty is thought to be the ancestral home of the apple, and
-        the wild <i lang="la">Malus sieversii</i> is considered a likely
-        candidate for the ancestor of the modern domestic apple.
-      </Panel>
+      {/* <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      /> */}
+      <h1>Time Passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>start</button>
+      <button onClick={handleStop}>stop</button>
     </>
   );
 }
+
+// let nextId = 3;
+// const initialTasks = [
+//   { id: 0, text: "Visit Kafka Museum", done: true },
+//   { id: 1, text: "Watch a puppet show", done: false },
+//   { id: 2, text: "Lennon Wall pic", done: false },
+// ];
